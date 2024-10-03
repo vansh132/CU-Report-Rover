6b2a67d58684b3a48c69aa6aa72899f85074268a
Vansh Shah <shah.vansh132@gmail.com>
2024-10-03 23:25:04 +0530

    pdf fonts changed

--- lib/devices.dart
+++ lib/devices.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:report/auto_report.dart';
-import 'package:report/mobile_auto_report.dart';
+import 'package:report/temp.dart';
 
 class Devices extends StatelessWidget {
   const Devices({super.key});
@@ -9,6 +9,6 @@ class Devices extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
-    return width >= 750 ? const AutoReport() : const MobileAutoReport();
+    return width >= 750 ? const AutoReport() : const TemporaryDown();
   }
 }
